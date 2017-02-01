9c9e7867cec3fbed00097cc2d426ea54cdcc0152
Ophir LOJKINE <pere.jobs@gmail.com>
2017-02-01 17:08:52 +0300

    reformat with elm-format

--- Choices.elm
+++ Choices.elm
@@ -1,9 +1,19 @@
-module Choices exposing (
-    Model, Answer, makeModel, makeModelFromTuples, getAllSelected, getSelected, setSelected,
-    Msg, update,
-    ViewParams, ViewType(..), view, genericView
-  )
-
+module Choices
+    exposing
+        ( Model
+        , Answer
+        , makeModel
+        , makeModelFromTuples
+        , getAllSelected
+        , getSelected
+        , setSelected
+        , Msg
+        , update
+        , ViewParams
+        , ViewType(..)
+        , view
+        , genericView
+        )
 
 {-| This library allows you to draw an HTML GUI for choices  between different values
 
@@ -23,20 +33,28 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import String
 
+
 main =
-  Html.beginnerProgram { model = exampleModel, view = view, update = update }
+    Html.beginnerProgram { model = exampleModel, view = view, update = update }
+
 
 
 -- MODEL
-{-| Represents a choice (one of the possible answers) and its human description-}
-type alias Answer valueType = {
-  value: valueType,
-  selected:Bool,
-  description:String
-}
 
-{-| A list of answsers (or choices) -}
-type alias Model valueType = List (Answer valueType)
+
+{-| Represents a choice (one of the possible answers) and its human description
+-}
+type alias Answer valueType =
+    { value : valueType
+    , selected : Bool
+    , description : String
+    }
+
+
+{-| A list of answsers (or choices)
+-}
+type alias Model valueType =
+    List (Answer valueType)
 
 
 {-| Make a model from a list of values a function associating a value to its description
@@ -50,26 +68,35 @@ type alias Model valueType = List (Answer valueType)
     makeModel snd [(True, "Yes"), (False, "No")]
 -}
 makeModel : (a -> String) -> List a -> Model a
-makeModel stringify = List.map
-                        (\v -> {
-                          value = v,
-                          selected = False,
-                          description = stringify v
-                        })
+makeModel stringify =
+    List.map
+        (\v ->
+            { value = v
+            , selected = False
+            , description = stringify v
+            }
+        )
+
+
 {-| Make a model from a list of (value, description) tuples
 
     makeModelFromTuples  [(True, "Yes"), (False, "No")]
 -}
-makeModelFromTuples : List (a, String) -> Model a
-makeModelFromTuples = List.map
-                        (\(value, description) -> {
-                          value = value,
-                          selected = False,
-                          description = description
-                        })
+makeModelFromTuples : List ( a, String ) -> Model a
+makeModelFromTuples =
+    List.map
+        (\( value, description ) ->
+            { value = value
+            , selected = False
+            , description = description
+            }
+        )
+
 
 exampleModel : Model Bool
-exampleModel = makeModelFromTuples [(True, "Yes"), (False, "No")]
+exampleModel =
+    makeModelFromTuples [ ( True, "Yes" ), ( False, "No" ) ]
+
 
 {-| get a list of all answers that have been selected
 
@@ -82,7 +109,9 @@ exampleModel = makeModelFromTuples [(True, "Yes"), (False, "No")]
         == [1,3]
 -}
 getAllSelected : Model valueType -> List valueType
-getAllSelected = List.filter (.selected) >> List.map (.value)
+getAllSelected =
+    List.filter (.selected) >> List.map (.value)
+
 
 {-| get the first selected answer
 
@@ -101,37 +130,56 @@ getAllSelected = List.filter (.selected) >> List.map (.value)
         == Just 1
 -}
 getSelected : Model valueType -> Maybe valueType
-getSelected = getAllSelected >> List.head
+getSelected =
+    getAllSelected >> List.head
 
-{-|Set the set of selected elements-}
+
+{-| Set the set of selected elements
+-}
 setSelected : List valueType -> Model valueType -> Model valueType
 setSelected selection =
-  List.map (\answer -> {answer | selected = List.member answer.value selection})
+    List.map (\answer -> { answer | selected = List.member answer.value selection })
+
+
 
 -- UPDATE
 
-{-| Indicate how to update the model-}
+
+{-| Indicate how to update the model
+-}
 type Msg valueType
     = Check valueType Bool Bool
 
-{-| update the model -}
+
+{-| update the model
+-}
 update : Msg valueType -> Model valueType -> Model valueType
 update action model =
-  case action of
-    Check value deselectOthers selected ->
-      List.map
-        (\elem -> if elem.value == value
-                    then {elem | selected = selected}
-                    else {elem | selected = if deselectOthers
-                                              then False
-                                              else elem.selected
-                         }
-        )
-        model
+    case action of
+        Check value deselectOthers selected ->
+            List.map
+                (\elem ->
+                    if elem.value == value then
+                        { elem | selected = selected }
+                    else
+                        { elem
+                            | selected =
+                                if deselectOthers then
+                                    False
+                                else
+                                    elem.selected
+                        }
+                )
+                model
+
 
 
 -- VIEW
-type alias ModelName = String
+
+
+type alias ModelName =
+    String
+
 
 {-| What Html element to use in order to draw the GUI.
   * **InputCheckbox** : Use `<input type='checkbox'>`
@@ -139,58 +187,82 @@ type alias ModelName = String
   * **HtmlSelect** *allowMultiple*: Use `<select [multiple]>`
 -}
 type ViewType
-      = InputCheckbox
-      | InputRadio
-      | HtmlSelect Bool
+    = InputCheckbox
+    | InputRadio
+    | HtmlSelect Bool
+
 
 {-| Parameters of a view
   * viewType : html element to use
   * name : name HTML element
 -}
-type alias ViewParams = {
-    viewType : ViewType,
-    name : String
-  }
+type alias ViewParams =
+    { viewType : ViewType
+    , name : String
+    }
 
-{-| A view that uses the InputRadio display, and that has its input name set to "ElmChoices" -}
-view :  Model valueType -> Html (Msg valueType)
-view = genericView (ViewParams InputRadio "ElmChoices")
+
+{-| A view that uses the InputRadio display, and that has its input name set to "ElmChoices"
+-}
+view : Model valueType -> Html (Msg valueType)
+view =
+    genericView (ViewParams InputRadio "ElmChoices")
+
 
 {-| Given a ViewParams and a Model, create the corresponding Html elements
 -}
 genericView : ViewParams -> Model valueType -> Html (Msg valueType)
 genericView params model =
-  (answerContainer params.viewType) (List.map (viewAnswer params.viewType params.name) model)
+    (answerContainer params.viewType) (List.map (viewAnswer params.viewType params.name) model)
+
 
 answerContainer viewType =
-  case viewType of
-      HtmlSelect isMultiple -> select [multiple isMultiple]
-      _ -> div []
+    case viewType of
+        HtmlSelect isMultiple ->
+            select [ multiple isMultiple ]
+
+        _ ->
+            div []
+
 
 viewAnswer : ViewType -> ModelName -> Answer valueType -> Html (Msg valueType)
 viewAnswer viewType name =
-  case viewType of
-    InputCheckbox   -> viewAnswerInput "checkbox" name
-    InputRadio      -> viewAnswerInput "radio" name
-    HtmlSelect mult -> viewAnswerSelect mult
+    case viewType of
+        InputCheckbox ->
+            viewAnswerInput "checkbox" name
+
+        InputRadio ->
+            viewAnswerInput "radio" name
+
+        HtmlSelect mult ->
+            viewAnswerSelect mult
+
 
 viewAnswerInput : String -> ModelName -> Answer valueType -> Html (Msg valueType)
 viewAnswerInput inputType modelName answer =
-  label [] [
-    input ([
-            type' inputType,
-            checked answer.selected,
-            onCheck (Check answer.value (inputType=="radio")),
-            value (toString answer.value)
-          ] ++ (if modelName == "" then [] else [name modelName]))
-          []
-    , text answer.description
-  ]
+    label []
+        [ input
+            ([ type' inputType
+             , checked answer.selected
+             , onCheck (Check answer.value (inputType == "radio"))
+             , value (toString answer.value)
+             ]
+                ++ (if modelName == "" then
+                        []
+                    else
+                        [ name modelName ]
+                   )
+            )
+            []
+        , text answer.description
+        ]
+
 
 viewAnswerSelect : Bool -> Answer valueType -> Html (Msg valueType)
 viewAnswerSelect isMultiple answer =
-  option [
-          selected answer.selected,
-          onCheck (Check answer.value (not isMultiple)),
-          value (toString answer.value)
-         ] [text answer.description]
+    option
+        [ selected answer.selected
+        , onCheck (Check answer.value (not isMultiple))
+        , value (toString answer.value)
+        ]
+        [ text answer.description ]
